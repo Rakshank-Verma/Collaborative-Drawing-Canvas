4ad483bf5a7c3de69f447583d68c37a7c2cc0035
Rakshank Verma <rakshankverma130250@gmail.com>
2024-01-18 23:52:27 +0530

    Drawing successful

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import { useState, useRef, useEffect } from "react";
 
 const App = () => {
-  const canvasRef = useRef(null);
+  let canvasRef = useRef(null);
+  const contextRef = useRef(null);
   const [pos, setPos] = useState({ x: 0, y: 0 });
   const [isDrawing, setIsDrawing] = useState(false);
 
-  const draw = (e) => {};
+  const draw = (e) => {
+    if (isDrawing) {
+      contextRef.current.beginPath();
+      contextRef.current.moveTo(pos.x, pos.y);
+      pos.x = e.clientX;
+      pos.y = e.clientY;
+      // setPos((prePos) => ({ ...prePos, x: e.clientX, y: e.clientY }));
+      contextRef.current.lineTo(pos.x, pos.y);
+      contextRef.current.stroke();
+    }
+  };
 
   useEffect(() => {
-    const canvas = document.getElementById("canvas")
-    const context = canvas.getContext("2d");
+    const canvas = canvasRef.current;
     canvas.style.backgroundColor = "#274c43";
-    canvas.style.width = "500px";
-    canvas.style.height = "300px";
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const context = canvas.getContext("2d");
+    context.lineCap = "round";
+    context.strokeStyle = "white";
+
+    context.lineWidth = 5;
+    contextRef.current = context;
   }, []);
 
-  return <canvas id="canvas" ref={canvasRef} onClick={draw} />;
+  return (
+    <canvas
+      id="canvas"
+      ref={canvasRef}
+      onMouseDown={(e) => {
+        // console.log(e.nativeEvent);
+        setPos((prePos) => ({ ...prePos, x: e.clientX, y: e.clientY }));
+        setIsDrawing((prev) => (prev = true));
+      }}
+      onMouseUp={() => setIsDrawing((prev) => (prev = false))}
+      onMouseMove={draw}
+    />
+  );
 };
 
 export default App;
